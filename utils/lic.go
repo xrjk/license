46429f91155eb0883e795d6fae771e78b52c9eae
xrjk <xrjk@msn.com>
2021-12-13 00:03:57 +0800

    license check 1 hour

--- utils/lic.go
+++ utils/lic.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"github.com/denisbrodbeck/machineid"
-
 )
 
 type AppLicenseInfo struct {
@@ -36,30 +35,32 @@ func EncryptLic(appInfoFile, key string) {
 		panic(err)
 	}
 	conf := AppLicenseInfo{}
-	if err := json.Unmarshal(contentByte, &conf); err == nil {
-		tmpText := string(contentByte)
-		//进行加密
-		tmpText = AesEncrypt(tmpText, key)
+	for {
+		if err := json.Unmarshal(contentByte, &conf); err == nil {
+			tmpText := string(contentByte)
+			//进行加密
+			tmpText = AesEncrypt(tmpText, key)
 
-		//生成license授权文件
-		currentDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-		if err != nil {
-			log.Fatal(err)
-		}
-		lic_file_path := currentDir + string(os.PathSeparator) + "license"
-		log.Println(lic_file_path)
-		lic_file_path = "license"
-		dstFile, err := os.Create(lic_file_path)
-		if err != nil {
-			log.Fatal(err)
-		}
+			//生成license授权文件
+			currentDir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+			if err != nil {
+				log.Fatal(err)
+			}
+			lic_file_path := currentDir + string(os.PathSeparator) + "license"
+			log.Println(lic_file_path)
+			lic_file_path = "license"
+			dstFile, err := os.Create(lic_file_path)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-		dstFile.WriteString(tmpText)
-		dstFile.Close()
-	} else {
-		fmt.Println(err)
+			dstFile.WriteString(tmpText)
+			dstFile.Close()
+		} else {
+			fmt.Println(err)
+		}
+		time.Sleep(1 * time.Minute)
 	}
-
 }
 
 func ValidAppLic(appInfoFile, key string) {
@@ -78,33 +79,39 @@ func ValidAppLic(appInfoFile, key string) {
 	//进行解密
 	tmpText = AesDecrypt(tmpText, key)
 	conf := AppLicenseInfo{}
-	if err := json.Unmarshal([]byte(tmpText), &conf); err == nil {
+	for {
+		fmt.Println("授权文件检查")
+		if err := json.Unmarshal([]byte(tmpText), &conf); err == nil {
 
-		//获取本机的ID
-		id, err := machineid.ID()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		if conf.ObjUUID != id {
-			fmt.Println("授权失败,错误代码01")
-			os.Exit(0)
-
-		}
-
-		limitedTime := conf.LimitedTime
-
-		if limitedTime != "" {
-			licDate, _ := strconv.Atoi(limitedTime)
-			nowDate := time.Now().Format("20060102")
-			currentDate, _ := strconv.Atoi(nowDate)
-			if licDate < currentDate {
-				log.Print("[警告] 授权文件已过期!!!授权结束日期:", licDate)
-				os.Exit(0)
+			//获取本机的ID
+			id, err := machineid.ID()
+			if err != nil {
+				log.Fatal(err)
 			}
-		}
 
-	} else {
-		log.Fatal(err)
+			if conf.ObjUUID != id {
+				fmt.Println("授权失败,错误代码01")
+				os.Exit(0)
+
+			}
+
+			limitedTime := conf.LimitedTime
+
+			if limitedTime != "" {
+				licDate, _ := strconv.Atoi(limitedTime)
+				nowDate := time.Now().Format("20060102")
+				currentDate, _ := strconv.Atoi(nowDate)
+				if licDate < currentDate {
+					log.Print("[警告] 授权文件已过期!!!授权结束日期:", licDate)
+					os.Exit(0)
+				}
+			}
+
+		} else {
+			log.Print("[警告] 授权文件不正确，请联系客服!!!")
+			os.Exit(0)
+		}
+		time.Sleep(1 * time.Hour)
 	}
+
 }
